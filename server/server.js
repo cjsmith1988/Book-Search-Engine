33dfe0a2bf9b4d90a2fdded8941846d9a5f9af54
Curtis Smith <cj1988333@hotmail.com>
2021-05-05 12:44:43 -0400

    heroku update

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -27,7 +29,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
